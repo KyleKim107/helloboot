adfc47df927c2a12c6aa4670292ba5e8c5d4d555
Kyle Kim <dkim577@wisc.edu>
2023-05-29 22:09:44 +0900

    Spring Boot이 제공하던 Main메소드를 처음부터 빌드 해보기 시작 part 2
    -  서브릿 등록
    -- 서브릿 등록 후 서브릿이 뜬다.

--- src/main/java/com/kylesboot/helloboot/HellobootApplication.java
+++ src/main/java/com/kylesboot/helloboot/HellobootApplication.java
@@ -1,18 +1,33 @@
 package com.kylesboot.helloboot;
 
-
-import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class HellobootApplication {
 
     public static void main(String[] args) {
         System.out.println("Hello Containerless StandAlone Server");
 
-        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory(); // 톰캣 서브릿 웹서버를 만들어준다.
+        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory(); // Embeded 톰캣 서브릿 웹서버를 호출한.
         // 만약 다른 서브렛 컨테이너 를 사용하고 싶다면 JettyServletWebServerFactory()
-        WebServer webServer = serverFactory.getWebServer();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("Hello", new HttpServlet() { // 서브랫 추가해주기
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    resp.setStatus(200); // set status
+                    resp.setHeader("Content-Type", "text/plain"); // set header
+                    resp.getWriter().print("Hello Servlet"); // set Body
+                }
+            }).addMapping("/hello"); //요청이 들어오면 매핑을 해준다.
+        });
         webServer.start();
     }
 
