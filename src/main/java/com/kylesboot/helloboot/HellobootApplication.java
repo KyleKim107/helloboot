768e7f4bfb082ac9a4c708e5d2f2a0dbef204ca8
Kyle Kim <dkim577@wisc.edu>
2023-05-29 23:42:48 +0900

    Spring Boot이 제공하던 Main메소드를 처음부터 빌드 해보기 시작 part 3
    - Front Controller에서HelloController.java로 분산시키기
    - Mappring과 binding

--- src/main/java/com/kylesboot/helloboot/HellobootApplication.java
+++ src/main/java/com/kylesboot/helloboot/HellobootApplication.java
@@ -24,17 +24,22 @@ public class HellobootApplication {
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory(); // Embeded 톰캣 서브릿 웹서버를 호출한.
         // 만약 다른 서브렛 컨테이너 를 사용하고 싶다면 JettyServletWebServerFactory()
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            HelloController helloController = new HelloController();
+
             servletContext.addServlet("frontcontroller", new HttpServlet() { // 서브랫 추가해주기
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
                     // 인증, 보안, 다국어, 공통기능
+
+                    // hello와 GET요청을 서브랫과 매핑
                     if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) { // hello page와 Get요청일때만
 
-                        String name = req.getParameter("name");
+                        String name = req.getParameter("name"); // 매핑
+                        String printValue = helloController.hello(name);// 비즈니스 로직 바인딩
 
                         resp.setStatus(HttpStatus.OK.value()); // set status
                         resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE); // set header
-                        resp.getWriter().print("Hello " + name); // set Body
+                        resp.getWriter().print(printValue); // set Body
                     } else if(req.getRequestURI().equals("/user")){ // user page
                         //
                     }else{
