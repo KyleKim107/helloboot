b53089009c4a08212cbb6233c19f7f6d7504d6a4
Kyle Kim <dkim577@wisc.edu>
2023-05-29 23:21:40 +0900

    Spring Boot이 제공하던 Main메소드를 처음부터 빌드 해보기 시작 part 3
    -- Front Controller추가해준다.
    -- 페이지마다 요청을 다변화 해준다.

--- src/main/java/com/kylesboot/helloboot/HellobootApplication.java
+++ src/main/java/com/kylesboot/helloboot/HellobootApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -23,17 +24,24 @@ public class HellobootApplication {
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory(); // Embeded 톰캣 서브릿 웹서버를 호출한.
         // 만약 다른 서브렛 컨테이너 를 사용하고 싶다면 JettyServletWebServerFactory()
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("Hello", new HttpServlet() { // 서브랫 추가해주기
+            servletContext.addServlet("frontcontroller", new HttpServlet() { // 서브랫 추가해주기
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    String name = req.getParameter("name");
+                    // 인증, 보안, 다국어, 공통기능
+                    if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) { // hello page와 Get요청일때만
 
+                        String name = req.getParameter("name");
 
-                    resp.setStatus( HttpStatus.OK.value() ); // set status
-                    resp.setHeader( HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE); // set header
-                    resp.getWriter().print("Hello " + name); // set Body
+                        resp.setStatus(HttpStatus.OK.value()); // set status
+                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE); // set header
+                        resp.getWriter().print("Hello " + name); // set Body
+                    } else if(req.getRequestURI().equals("/user")){ // user page
+                        //
+                    }else{
+                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+                    }
                 }
-            }).addMapping("/hello"); //요청이 들어오면 매핑을 해준다.
+            }).addMapping("/*"); //프론트 컨트롤러는 모든 요청을 받아 드린다
         });
         webServer.start();
     }
