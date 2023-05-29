560e3531c1ca2da668dc07a968b33b70a444e7a6
Kyle Kim <dkim577@wisc.edu>
2023-05-29 21:26:57 +0900

    Spring Boot이 제공하던 Main메소드를 처음부터 빌드 해보기 시작 part 1
    - 서버가 뜨긴 하지만 localhost:8080으로 접속시 404 page not found error가 나온다.

--- src/main/java/com/kylesboot/helloboot/HellobootApplication.java
+++ src/main/java/com/kylesboot/helloboot/HellobootApplication.java
@@ -1,13 +1,19 @@
 package com.kylesboot.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+
 public class HellobootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HellobootApplication.class, args);
+        System.out.println("Hello Containerless StandAlone Server");
+
+        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory(); // 톰캣 서브릿 웹서버를 만들어준다.
+        // 만약 다른 서브렛 컨테이너 를 사용하고 싶다면 JettyServletWebServerFactory()
+        WebServer webServer = serverFactory.getWebServer();
+        webServer.start();
     }
 
 }
